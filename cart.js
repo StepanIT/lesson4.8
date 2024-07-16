05dc0e629f1223c4c28e740a15d526550f2cdc04
Степан <19stepan94@mail.ru>
2024-07-16 12:18:45 +0500

    переписал при помощи рекурсии игру угадай число

--- cart.js
+++ cart.js
@@ -1,51 +1,51 @@
-'use strict';
+// 'use strict';
 
-const cart = {
-  items: [],
-  count: 0,
+// const cart = {
+//   items: [],
+//   count: 0,
 
-  add(productName, priceProduct, quantityOfGoods = 1) {
-    const product = {
-      productName,
-      priceProduct,
-      quantityOfGoods,
-    };
-    this.items.push(product);
-    this.increaseCount();
-  },
+//   add(productName, priceProduct, quantityOfGoods = 1) {
+//     const product = {
+//       productName,
+//       priceProduct,
+//       quantityOfGoods,
+//     };
+//     this.items.push(product);
+//     this.increaseCount();
+//   },
 
-  increaseCount() {
-    this.count = this.items.reduce((acc, {quantityOfGoods}) =>
-      acc + quantityOfGoods, 0);
-  },
+//   increaseCount() {
+//     this.count = this.items.reduce((acc, {quantityOfGoods}) =>
+//       acc + quantityOfGoods, 0);
+//   },
 
-  calculateItemPrice() {
-    return this.items.reduce((acc, item) =>
-      acc + (item.priceProduct * item.quantityOfGoods), 0);
-  },
+//   calculateItemPrice() {
+//     return this.items.reduce((acc, item) =>
+//       acc + (item.priceProduct * item.quantityOfGoods), 0);
+//   },
 
-  get totalPrice() {
-    return this.calculateItemPrice();
-  },
+//   get totalPrice() {
+//     return this.calculateItemPrice();
+//   },
 
-  clear() {
-    this.items.length = 0;
-    this.totalPrice = 0;
-    this.count = 0;
-  },
+//   clear() {
+//     this.items.length = 0;
+//     this.totalPrice = 0;
+//     this.count = 0;
+//   },
 
-  print() {
-    console.log(`${JSON.stringify(this.items)}
-       ${this.totalPrice}`);
-  },
-};
+//   print() {
+//     console.log(`${JSON.stringify(this.items)}
+//        ${this.totalPrice}`);
+//   },
+// };
 
 
-const keys = Object.keys(cart);
-console.log(keys);
+// const keys = Object.keys(cart);
+// console.log(keys);
 
-cart.add('Микроволновка', 7000, 15);
-cart.add('Чайник', 2000, 5);
-cart.add('Диван', 40000, 3);
-cart.add('Стол', 10500, 6);
-cart.print();
+// cart.add('Микроволновка', 7000, 15);
+// cart.add('Чайник', 2000, 5);
+// cart.add('Диван', 40000, 3);
+// cart.add('Стол', 10500, 6);
+// cart.print();
---
+++ game01.js
@@ -0,0 +1,31 @@
+'use strict'
+
+
+const foo = (number) => {
+    const randomNumber = Math.round(Math.random() * 100);
+    console.log(randomNumber);
+    var number = prompt('Введите число от 1 до 100');
+
+    if (randomNumber === null) {
+        return foo(number);
+    }
+
+    if (isNaN(number)) {
+        alert('Введи число!');
+        return foo(number);
+    }
+
+    if (number > randomNumber) {
+        alert('Меньше! ' + ' Введите новый вариант ');
+        return foo(number);
+    }
+
+    if (number < randomNumber) {
+        alert('Больше! ' + ' Введите число ');
+        return foo(number);
+    }
+    alert('Правильно!');
+    return 'Вы победили';
+}
+
+console.log(foo());
