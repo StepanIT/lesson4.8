d5ca8a48d863cef6474bb5f666559f44db232e08
Степан Рудаков <19stepan94@mail.ru>
2024-07-18 15:50:27 +0500

    сделал четвертую задачу в 4.11

--- cart.js
+++ cart.js
@@ -1,56 +1,71 @@
-// 'use strict';
+'use strict';
 
-// const cart = {
-//   items: [],
-//   count: 0,
-
-//   add(productName, priceProduct, quantityOfGoods = 1) {
-//     const product = {
-//       productName,
-//       priceProduct,
-//       quantityOfGoods,
-//     };
-//     this.items.push(product);
-//     this.increaseCount();
-//   },
-
-//   increaseCount() {
-//     this.count = this.items.reduce((acc, {quantityOfGoods}) =>
-//       acc + quantityOfGoods, 0);
-//   },
-
-//   calculateItemPrice() {
-//     return this.items.reduce((acc, item) =>
-//       acc + (item.priceProduct * item.quantityOfGoods), 0);
-//   },
-
-//   get totalPrice() {
-//     return this.calculateItemPrice();
-//   },
-
-//   clear() {
-//     this.items.length = 0;
-//     this.totalPrice = 0;
-//     this.count = 0;
-//   },
-
-//   print() {
-//     console.log(`${JSON.stringify(this.items)}
-//        ${this.totalPrice}`);
-//   },
-// };
+const cart = {
+  items: [],
+  count: 0,
+  discount: 0,
 
 
-// const keys = Object.keys(cart);
-// console.log(keys);
+  get setDiscount() {
+    return this.discount;
+  },
 
-// cart.add('Микроволновка', 7000, 15);
-// cart.add('Чайник', 2000, 5);
-// cart.add('Диван', 40000, 3);
-// cart.add('Стол', 10500, 6);
-// cart.print();
+  set setDiscount(promocode) {
+    if (promocode === 'METHED') {
+      this.discount = 15;
+    }
+    if (promocode === 'NEWYEAR') {
+      this.discount = 21;
+    }
+  },
+
+  add(productName, priceProduct, quantityOfGoods = 1) {
+    const product = {
+      productName,
+      priceProduct,
+      quantityOfGoods,
+    };
+    this.items.push(product);
+    this.increaseCount();
+  },
+
+  increaseCount() {
+    this.count = this.items.reduce((acc, {quantityOfGoods}) =>
+      acc + quantityOfGoods, 0);
+  },
+
+  calculateItemPrice() {
+    return this.items.reduce((acc, item) =>
+      acc + (item.priceProduct * item.quantityOfGoods) -
+    (item.priceProduct * item.quantityOfGoods) * this.discount / 100, 0);
+  },
+
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
+
+  clear() {
+    this.items.length = 0;
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+
+  print() {
+    console.log(`${JSON.stringify(this.items)}
+       ${this.totalPrice}`);
+  },
 
 
+};
 
 
+const keys = Object.keys(cart);
+console.log(keys);
 
+
+cart.setDiscount = 'NEWYEAR';
+cart.add('Микроволновка', 7000, 15);
+cart.add('Чайник', 2000, 5);
+cart.add('Диван', 40000, 3);
+cart.add('Стол', 10500, 6);
+cart.print();
