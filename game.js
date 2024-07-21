a2dc25b60027be09603095002efafeea3e0bba6d
Степан Рудаков <19stepan94@mail.ru>
2024-07-21 20:45:51 +0500

    сделал задание 4.12

--- game.js
+++ game.js
@@ -28,27 +28,63 @@
 
         return function start() {
             
-            let userChoice = prompt('камень, ножницы или бумага?');
-            
-            if(userChoice === ''){
-                alert('Выберите из трёх вариантов');
-            }
-            if(userChoice === null){
-                confirm('Вы точно хотите выйти?');
-                if(userChoice === ''){
-                    return
-                }
-                
-            };
-            
 
+
+          const chooseWinner = (userChoice, computerChoice) => {
+
+            if (userChoice === computerChoice) {
+              return 'Ничья!';
+            } else if (
+              (userChoice === 'камень' && computerChoice === 'ножницы') ||
+              (userChoice === 'ножницы' && computerChoice === 'бумага') ||
+              (userChoice === 'бумага' && computerChoice === 'камень')
+            ) {
+              result.player++;
+              return 'Вы победили!';
+            } else {
+              result.computer++;
+              return 'Вы проиграли!';
+            }
+          };
+
+
+
+      
+
+
+
+
+
+
+            const playRound = () => {
+              const userChoice = prompt('камень, ножницы или бумага?');
+              if (userChoice === null) {
+                const confirmExit = confirm('Точно ли вы хотите выйти?');
+                if (confirmExit === true) {
+                  alert(`Игра окончена. Ваш счет: ${result.player}, Счет компьютера: ${result.computer}`);
+                  return;
+                } else {
+                  return playRound();
+                };
+              };
+              if(!['камень', 'ножницы', 'бумага'].includes(userChoice)) {
+                alert('Некорректный выбор. Попробуйте снова.');
+                return playRound();
+              };
+      
             const computerChoice = FIGURES_RUS[getRandomIntInclusive()];
-            
-        
+            alert(`Вы выбрали: ${userChoice}`);
+            alert(`Компьютер выбрал: ${computerChoice}`);
+            alert(chooseWinner(userChoice, computerChoice));
+
 
             return start();
 };
-
+            return playRound();
 }; 
+    };
+
+
+    
     window.RPS = game;
 })();
